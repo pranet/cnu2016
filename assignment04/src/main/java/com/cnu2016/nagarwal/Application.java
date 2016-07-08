cc1b8ec930f61b7ff22bbe1b3d03f4b9e0ea8cb4
Nitesh Agarwal <niteshagarwal002@Niteshs-MacBook-Pro.local>
2016-07-08 18:01:37 +0400

    Assignment 4 - Milestone 1 Completed

--- assignment04/src/main/java/com/cnu2016/nagarwal/Application.java
+++ assignment04/src/main/java/com/cnu2016/nagarwal/Application.java
@@ -3,10 +3,70 @@ package com.cnu2016.nagarwal;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import java.util.Properties;
+
 @SpringBootApplication
 public class Application {
 
     public static void main( String[] args ){
+
+//        String fileName = "/Users/niteshagarwal002/git_repos/cnu2016/assignment04/target/maven-archiver/application.properties";
+//        InputStream propertyFile = null;
+//        Properties properties = new Properties();
+//
+//        try
+//        {
+//            ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
+//            URL propertyFileUrl = classLoader.getResource(fileName);
+//            if (propertyFileUrl != null)
+//            {
+//                propertyFile = propertyFileUrl.openStream();
+//
+//                if (propertyFile != null)
+//                {
+//                    if (fileName.toUpperCase().endsWith(".XML"))
+//                    {
+//                        properties.loadFromXML(propertyFile);
+//                    }
+//                    else
+//                    {
+//                        properties.load(propertyFile);
+//                    }
+//                }
+//                else
+//                {
+//                    System.err.println("The property file [" + fileName + "] could not " +
+//                            "be opened for reading.");
+//                }
+//            }
+//            else
+//            {
+//                System.err.println("The property file [" + fileName + "] could not " +
+//                        "be found in the classpath.");
+//            }
+//        }
+//        catch (IOException e)
+//        {
+//            System.err.println ("The property file [" + fileName + "] could not " +
+//                    "be loaded. The error is: " + e.toString());
+//        }
+//        finally
+//        {
+//            if (propertyFile != null)
+//            {
+//                try
+//                {
+//                    propertyFile.close();
+//                }
+//                catch (IOException e)
+//                {
+//                    e.printStackTrace();
+//                }
+//            }
+//        }
 
         SpringApplication.run(Application.class);
     }
