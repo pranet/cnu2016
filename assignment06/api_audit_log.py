c9ed87c60c8b6901e0498b2e023e6ba1630886d1
Nitesh Agarwal <niteshagarwal002@Niteshs-MacBook-Pro.local>
2016-07-12 20:16:04 +0400

    Assignment 6 milestone 1 done

--- assignment06/api_audit_log.py
+++ assignment06/api_audit_log.py
@@ -15,23 +15,27 @@ class audit_log(db.Model):
     parameters = db.Column(db.VARCHAR)
     response = db.Column(db.Integer)
     ipAddress = db.Column(db.VARCHAR)
+    requestType = db.Column(db.VARCHAR)
+    requestDuration = db.Column(db.Integer)
 
-    def __init__(self, audit_time_stamp,  url, parameters, response, ip_address):
+    def __init__(self, audit_time_stamp,  url, parameters, response, ip_address, requestType, requestDuration):
         self.auditTimeStamp = audit_time_stamp
         self.url = url
         self.parameters = parameters
         self.response = response
         self.ipAddress = ip_address
+        self.requestType = requestType
+        self.requestDuration = requestDuration
 
     def as_json(self):
         return {
-            "idAuditLog":self.idAuditLog,
-            "auditTimeStamp": HelperFunctions.format_date_to_iso_format(self.auditTimeStamp),
-            # "auditTimeStamp": self.auditTimeStamp,
-            "url":self.url,
-            "parameters":self.parameters,
-            "response":self.response,
-            "ipAddress": self.ipAddress
+            "timestamp": HelperFunctions.format_date_to_iso_format(self.auditTimeStamp),
+            "url": self.url,
+            "request_type": self.requestType,
+            "parameters": HelperFunctions.remove_braces(str(self.parameters)),
+            "request_duration_ms": self.requestDuration,
+            "response_code":  self.response,
+            "request_ip_address": self.ipAddress
         }
 
 
@@ -40,22 +44,16 @@ class audit_log(db.Model):
 
 @app.route("/api/auditLogs", methods=['GET'])
 def get_audit_logs():
-    startTime = request.args.get('startTime')
-    endTime = request.args.get('endTime')
-    lim = request.args.get('limit', 10)
+    startTime = request.args.get('startTime', '2015-01-01 00:00:00')
+    endTime = request.args.get('endTime', '3000-12-10 00:00:00')
+    startTime = parser.parse(startTime)
+    endTime = parser.parse(endTime)
+    lim = min(10, int(request.args.get('limit', 10)))
     off = request.args.get('offset', 0)
-    if startTime is not None and endTime is not None:
-        startTime = parser.parse(startTime)
-        endTime = parser.parse(endTime)
-        return jsonify(data=[al.as_json() for al in audit_log.query
-                       .order_by(audit_log.auditTimeStamp.desc())
-                       .filter(audit_log.auditTimeStamp.between(startTime, endTime))
-                       .limit(lim).offset(off).all()])
-    else:
-        return jsonify(data=[al.as_json() for al in audit_log.query
-                       .order_by(audit_log.auditTimeStamp.desc())
-                       .limit(lim).offset(off).all()])
-
+    return jsonify(data=[al.as_json() for al in audit_log.query
+                   .order_by(audit_log.auditTimeStamp.desc())
+                   .filter(audit_log.auditTimeStamp.between(startTime, endTime))
+                   .limit(lim).offset(off).all()])
 
 
 if __name__ == '__main__':
--- assignment06/helpers.py
+++ assignment06/helpers.py
@@ -1,12 +1,14 @@
 from dateutil import parser
+import re
 import datetime
 class HelperFunctions:
     @staticmethod
     def format_date_to_iso_format(date):
         date = str(date)
         formatted_date = parser.parse(date)
-        return formatted_date.isoformat()
+        formatted_date = formatted_date.isoformat()
+        formatted_date = datetime.datetime.strptime(str(formatted_date), '%Y-%m-%dT%H:%M:%S').strftime('%m/%d/%yT%H:%M:%S')
+        return formatted_date
     @staticmethod
-    def iso_format_to_default_format(timestamp):
-        converted_date = datetime.datetime(timestamp)
-        return converted_date
+    def remove_braces(line):
+        return re.sub('[{}]', '', line)
